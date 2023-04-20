b119a66c763cecf4e634b9fb0a0c60891fac0f2c
ApurvaDate <102308153+ApurvaDate@users.noreply.github.com>
2023-04-20 17:27:00 +0530

    Add files via upload

---
+++ login_signup_sytem.py
@@ -0,0 +1,16 @@
+#Building a simplelogin and sign up system
+print("Create account now")
+username=input('Enter username: ')
+password=input("Enter password: ")
+
+print('Your account has been created successfully')
+
+print('Login now')
+
+username2=input('Enter username: ')
+password2=input('Enter password: ')
+
+if username==username2 and password==password2:
+    print('Logged in successfully')
+else:
+    print("Invalid credentials")
---
+++ python_shell.py
@@ -0,0 +1,53 @@
+print("Hope you are enjoying this tutorial")
+Hope you are enjoying this tutorial
+name='tim'
+print(name)
+tim
+name
+'tim'
+for letter in 'tim':
+    print(letter)
+
+    
+t
+i
+m
+>>> class person();
+SyntaxError: incomplete input
+>>> class person():
+...     name="John"
+... 
+...     
+>>> p1=person()
+>>> p1.name
+'John'
+>>> if p1.name="John":
+...     
+SyntaxError: cannot assign to attribute here. Maybe you meant '==' instead of '='?
+>>> 2+3
+5
+>>> 9-0
+9
+>>> def say_hi(name):
+...     print(name)
+... 
+...     
+>>> say_hi(apurva)
+Traceback (most recent call last):
+  File "<pyshell#20>", line 1, in <module>
+    say_hi(apurva)
+NameError: name 'apurva' is not defined
+>>> say_hi("apurva")
+apurva
+>>> try:
+...     age=int(input("Enter age: ")
+... except:
+...     
+SyntaxError: '(' was never closed
+>>> try:
+...     age=int(input("Enter age: "))
+... except:
+...     print("no age given")
+... 
+Enter age: 23
+>>> 
